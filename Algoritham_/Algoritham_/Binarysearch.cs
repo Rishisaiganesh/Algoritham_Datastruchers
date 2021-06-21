c1469748a0cbe63dd3573f2b48c39a96a8ba13b1
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-21 21:40:52 +0530

    [Rishi]Adding:BinarySearch

---
+++ Algoritham_/Algoritham_/Binarysearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritham_
+{
+    public class Binarysearch
+    {
+        public int Binary(string[] Array, string value)
+        {
+            int leftValue = 0, RightValue = Array.Length - 1;
+            while (leftValue <= RightValue)
+            {
+                int middleValue = leftValue + RightValue / 2;
+                int find = value.CompareTo(Array[middleValue]);
+                if (find == 0)
+                    return middleValue;
+                if (find > 0)
+                    return middleValue + 1;
+                else
+                    return middleValue - 1;
+            }
+            return -1;
+        }
+    }
+}
--- Algoritham_/Algoritham_/Program.cs
+++ Algoritham_/Algoritham_/Program.cs
@@ -7,11 +7,35 @@ namespace Algoritham_
         static void Main(string[] args)
         {
             Console.WriteLine("Algoritham");
-            Permutations obj = new Permutations();
-            string s = "ABCD";
-            Permutations.PermutationRecursive(s, " ");
-            Console.WriteLine(" ");
-            Permutations.PermutationInterative(s, 0, s.Length - 1);
+            Console.WriteLine("Press 1 to Permutation");
+            Console.WriteLine("press 2 to BinarySearch");
+            int check = Convert.ToInt32(Console.ReadLine());
+            switch (check)
+            {
+                case 1:
+                    //Permentation Problem
+                    string s = "ABCD";
+                    Permutations.PermutationRecursive(s, " ");
+                    Console.WriteLine(" ");
+                    Permutations.PermutationInterative(s, 0, s.Length - 1);
+                    break;
+                case 2:
+                    //binary search 
+                    Console.WriteLine("Hi every One GoodMorning Have a Nice Day" +"Give any Key" );
+                    string[] Array = { "Hi", "every", "One", "GoodMorning","Have aNice Day" };
+                    string value = Convert.ToString(Console.ReadLine());
+                    Binarysearch binary = new Binarysearch();
+                    int find = binary.Binary(Array, value);
+                    if (find == -1)
+                        Console.WriteLine("String Not Found");
+                    else
+                        Console.WriteLine("string is found" + find);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option Enter valid Option");
+                    break;
+            }
+
 
         }
     }
