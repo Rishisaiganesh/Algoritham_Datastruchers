e7d245113cb778a3554baf8d47a940b903ce9969
Rishisaiganesh <crazyganesh67@gmail.com>
2021-06-26 17:35:07 +0530

    [Rishi]Adding Booblesort

---
+++ Algoritham_/Algoritham_/Bubble.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algoritham_
+{
+   public class Bubble
+    {
+        int i, j;
+        int[] Array = { 1, 2, 4, 3, 4, 5, 9, 7, 6, 8 };
+        public void Bubblesort()
+        {
+            for (j = 0; j < Array.Length - 1; i++)
+            {
+                if (Array[j] > Array[j + 1])
+                {
+                    int temp = Array[j + 1];
+                    Array[j + 1] = temp;
+
+                }
+            }
+        }
+         foreach(var value in Array)
+         {
+               Console.WriteLine(value);
+         }    
+   }
+}
